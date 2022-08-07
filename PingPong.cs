305bc3b2ff2bb9bafc9949eb8e1df125391a02ff
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-06 23:47:20 -0700

    create script to collect number from user

--- PingPong.cs
+++ PingPong.cs
@@ -4,6 +4,8 @@ class Program
 {
   static void Main()
   {
-    
+    Console.WriteLine("Please enter a number.");
+    string stringUserInput = Console.ReadLine();
+    int inputNumber = int.Parse(stringUserInput);
   }
 }
