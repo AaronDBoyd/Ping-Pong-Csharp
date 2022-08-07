4b552fb55617aedb32664a96e1111f1293354877
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-06 23:55:30 -0700

    create loop to output numbers or ping pong response

--- PingPong.cs
+++ PingPong.cs
@@ -7,5 +7,25 @@ class Program
     Console.WriteLine("Please enter a number.");
     string stringUserInput = Console.ReadLine();
     int inputNumber = int.Parse(stringUserInput);
+
+    for (int i = 1; i <= inputNumber; i++) 
+    {
+      if (i % 3 == 0 && i % 5 == 0)
+      {
+        Console.WriteLine("Ping Pong");
+      }
+      else if (i % 5 == 0)
+      {
+        Console.WriteLine("Pong");
+      }
+      else if (i % 3 == 0)
+      {
+        Console.WriteLine("Ping");
+      }
+      else
+      {
+        Console.WriteLine(i);
+      }
+    }
   }
 }
